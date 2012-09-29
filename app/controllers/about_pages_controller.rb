2955c69b40ee58bd22aeb7b6b148777f3f6d0759
Eliana Feasley <e.feasley@gmail.com>
2012-09-28 23:18:32 -0500

    Initialized some structure for my homepage

---
+++ app/controllers/about_pages_controller.rb
@@ -0,0 +1,19 @@
+class AboutPagesController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def research
+  end
+
+  def projects
+  end
+
+  def cv
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homepage::Application.config.secret_token = 'a5cdeff40c0f8326b99eefbd4d7bb3dc1e85a81d3085e0791b991cea61224f11db48badc94f2da7cc1b13bbcb10d4c57167906e5c3ebb5cb03e9cd04f42a8f17'
